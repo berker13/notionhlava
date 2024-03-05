ad3cc587634b030ce36ab6de943f62a665f5565c
berker13 <148329668+berker13@users.noreply.github.com>
2024-03-05 22:50:48 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,18 +27,24 @@ class ToDoItemProps
     checked: Boolean;
 }
 
-        return (
-            <main className="px-4 py-8">
-                <div className="w-72 h-96 bg-white shadow-lg rounded-lg overflow-hidden">
-                    <div className="p-6">
-                        <h2 className="text-xl font-bold mb-2">Úloha 1</h2>
-                        <p className="text-gray-700 mb-4">Toto je první úloha, můžete ji upravit nebo smazat</p>
-                        <div className="flex justify-between">
-                            <button className="px-4 py-2 bg-blue-500 text-white rounded-md mr-2">Upravit</button>
-                            <button className="px-4 py-2 bg-gray-500 text-white rounded-md">Smazat</button>
-                        </div>
-                    </div>
-                </div>
-            </main>
-        )
+         return (
+        <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+            <div className="flex max-w-[980px] flex-col items-start gap-2">
+                <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+                    Beautifully Todo App.
+                </h1>
+                <p className="max-w-[700px] text-lg text-muted-foreground">
+                    Create your Todo list.
+                </p>
+            </div>
+            <TodoForm />
+            <ul className="flex h-full w-full flex-col overflow-hidden bg-popover text-popover-foreground rounded-lg border shadow-md">
+                {
+                    todos?.map((todo) => (
+                        <TodoList key={todo?.id} todo={todo} />
+                    ))
+                }
+            </ul>
+        </section>
+    )
     }
